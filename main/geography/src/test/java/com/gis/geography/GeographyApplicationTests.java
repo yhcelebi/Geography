916e4fcd9ba972783881871edd601670c8be4a13
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2023-07-05 14:31:40 +0300

    added spring boot

---
+++ main/geography/src/test/java/com/gis/geography/GeographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.geography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
